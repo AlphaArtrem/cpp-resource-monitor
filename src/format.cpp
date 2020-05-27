44cc39cec5c9364c748e505bfb1ebf64cfb30778
AlphaArtrem <yashawasthi01@gmail.com>
2020-05-27 23:22:09 +0530

    Fixed time formatting

--- src/format.cpp
+++ src/format.cpp
@@ -6,12 +6,12 @@ using std::string;
 
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds) {\
+string Format::ElapsedTime(long seconds) {
     string buffer = std::to_string(int(seconds / (60 * 60)));
-    string formattedTime = buffer.length() == 1 ? '0' + buffer : buffer + ':';
+    string formattedTime = (buffer.length() == 1 ? '0' + buffer : buffer) + ':';
     seconds %= 60 * 60;
     buffer = std::to_string(int(seconds / 60));
-    formattedTime += buffer.length() == 1 ? '0' + buffer : buffer + ':';
+    formattedTime += (buffer.length() == 1 ? '0' + buffer : buffer) + ':';
     seconds %= 60;
     buffer = std::to_string(int(seconds));
     formattedTime += buffer.length() == 1 ? '0' + buffer : buffer;
